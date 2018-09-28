5628dbcfc958b4c3c2745feb188039a9049d1077
Henry <henry81325@gmail.com>
2018-09-28 13:50:58 +0800

    user can update the normal/admin role

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,4 +6,15 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update(role: params[:role])
+      redirect_to admin_users_path
+      flash[:notice] = "role was successfully updated"
+    else
+      @users = User.all
+      render :index
+    end
+  end
+  
 end
