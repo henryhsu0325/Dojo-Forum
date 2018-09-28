5fbeec505be2fb816262107a38c80d08c121a2e9
Henry <henry81325@gmail.com>
2018-09-28 12:28:21 +0800

    setup for admin user/category index & fixed admin category classname

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,8 @@
-class Admin::CategoriessController < ApplicationController
+class Admin::CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
 
   def index
     @categories = Category.all
   end
+end
