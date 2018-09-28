95374cdf4c68e9bc30655721c146e7e004404d15
Henry <henry81325@gmail.com>
2018-09-28 15:19:10 +0800

    setup relation between user and post / post and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ApplicationRecord
   validates_uniqueness_of :name
 
   has_many :posts
+  # 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, PhotoUploader
+
+  belongs_to :user
+
+  has_many :categories
 end
