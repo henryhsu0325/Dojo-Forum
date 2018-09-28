e4550ea5ac6fd455d982a87dffa03d0b6928e02f
Henry <henry81325@gmail.com>
2018-09-28 14:24:27 +0800

    add new post & link all /new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index  
-  
+
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
   end
 end
