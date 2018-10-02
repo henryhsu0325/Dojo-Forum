a3707c85652fd8b767298e26d4b460845966d838
Henry <henry81325@gmail.com>
2018-10-02 12:09:39 +0800

    setup create new post & select permit and status

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,30 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = current_user.posts.build(post_params) 
+    if params[:commit] == "Submit"
+      @post.status = 'publish'
+      if @post.save
+        redirect_to post_path(@post)
+      else
+        render :new
+      end
+      elsif params[:commit] == "draft"
+        @post.status = 'draft'
+      if @post.save
+        redirect_to draft_user_path(@post)
+      else
+        render :new
+      end
+    end
+  end
+
   def feed
   end
 
   def post_params
-    params.require(:post).permit(:title, :description, :image)
+    params.require(:post).permit(:title, :description, :permit, :image)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, PhotoUploader
+  validates_inclusion_of :permit, :in => ["all", "friend", "myself"]
+  validates_inclusion_of :status, :in => ["publish", "draft"]
 
   belongs_to :user
 
