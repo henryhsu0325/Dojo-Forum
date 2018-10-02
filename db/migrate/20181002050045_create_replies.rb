cb71310ea14ba69ff3358d4088cc4b3ebd3b88a0
Henry <henry81325@gmail.com>
2018-10-02 13:02:48 +0800

    create reply model

---
+++ db/migrate/20181002050045_create_replies.rb
@@ -0,0 +1,12 @@
+class CreateReplies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :replies do |t|
+      t.text :comment
+      t.integer :post_id
+      t.integer :user_id      
+
+      t.timestamps
+    end
+    add_index :replies, [:post_id, :user_id]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181002033414) do
+ActiveRecord::Schema.define(version: 20181002050045) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20181002033414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.text "comment"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id", "user_id"], name: "index_replies_on_post_id_and_user_id"
   end
 
   create_table "users", force: :cascade do |t|
