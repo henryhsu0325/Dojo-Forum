b28a9cb9e1b6cd1b5840e0fb6ff14cd246f43f81
Henry <henry81325@gmail.com>
2018-10-03 09:25:34 +0800

    add pagination using Kaminari gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index  
 
   def index
-    @posts = Post.all_publish
+    @posts = Post.all_publish.page(params[:page]).per(10)
   end
 
   def new
