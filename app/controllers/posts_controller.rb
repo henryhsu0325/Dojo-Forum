64f9ad12a98d9f73c210500e92aa822590454e3e
Henry <henry81325@gmail.com>
2018-10-04 12:26:56 +0800

    edit & delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index  
+   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all_publish.page(params[:page]).per(20)
@@ -9,6 +10,7 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     @categories = Category.all
+    @category = @post.categories.ids
   end
 
   def create
@@ -30,7 +32,52 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+    if @post.user == current_user
+       @categories = Category.all
+    else
+      flash[:alert] = '沒有權限'
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      if params[:commit] == 'Published' && @post.draft == true
+        @post.draft = false
+        @post.save
+        flash[:notice] = '已成功發表'
+      else
+        flash[:notice] = '文章已更新'
+      end
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = @post.errors.full_messages.to_sentence if @post.errors.any?
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.user == current_user || current_user.admin?
+      @post.destroy
+      flash[:notice] = "Successfully deleted"
+      redirect_to posts_path
+    else
+      flash[:alert] = '沒有權限'
+      redirect_back(fallback_location: root_path)
+    end
+  end
+  
   def feed
+  end
+
+  private 
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
   def post_params
