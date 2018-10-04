10cab8ea5df44315c7ddcf1616c50eb9f16d0619
Henry <henry81325@gmail.com>
2018-10-05 00:23:12 +0800

    setup user routes (profile, user page)

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:show, :edit, :update] do
+    # profile/ user page
+    member do
+      get :posts
+      get :comments
+      get :collects
+      get :drafts
+      get :friends
+    end
+  end
+
   resources :posts do
     collection do
       get :feeds
     end
   end
+
   resources :categories, only: :show
   root "posts#index" 
 
@@ -14,6 +26,5 @@ Rails.application.routes.draw do
     resources :categories
     resources :users, only: [:index, :update]
     root "users#index"
-  end 
-end
-
+    end 
+  end
