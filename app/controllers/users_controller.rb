d324050d7da60c7ecf39ed9765efa2e5112748fb
Henry <henry81325@gmail.com>
2018-10-04 10:34:21 +0800

    user can update the profile details, upload avatar & style user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
-  def show
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def edit 
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
+
 end
