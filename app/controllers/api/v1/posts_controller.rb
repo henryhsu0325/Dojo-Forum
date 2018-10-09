4156677999c5347c907365fbd2ca50d38ebc0404
Henry <henry81325@gmail.com>
2018-10-09 14:53:41 +0800

    add show action for post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,26 @@
 class Api::V1::PostsController < ApplicationController
   
-  # GET http://localhost:3000/api/v1/photos
+  # GET http://localhost:3000/api/v1/posts
   def index
     @posts = Post.all_publish(current_user)
       render json: {
         data: @posts
       }
+  end
+
+  # GET http://localhost:3000/api/v1/posts/:id
+  def show
+    @post = Post.find_by(id: params[:id])
+    if !@post
+      render json: {
+        message: "Can't find the post!",
+        status: 400
+      }
+    else
+      render json: {
+        data: @post
+      }
+    end
   end
 
 end
