cfc0e684520b6c2a11a6842828afe6b02d2fe192
Henry <henry81325@gmail.com>
2018-10-09 14:44:59 +0800

    add index action for post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::PostsController < ApplicationController
+  
+  # GET http://localhost:3000/api/v1/photos
+  def index
+    @posts = Post.all_publish(current_user)
+      render json: {
+        data: @posts
+      }
+  end
+
 end
