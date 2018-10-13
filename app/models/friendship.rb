84b08148ea025cba5a0e9b977b2ff02303060858
Henry <henry81325@gmail.com>
2018-10-13 14:28:58 +0800

    add validates inclusion of status for friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ApplicationRecord
+  validates_inclusion_of :status, :in => ["send", "ignore", "connect"]
   validates :friend_id, uniqueness: { scope: :user_id }
 
   belongs_to :user
