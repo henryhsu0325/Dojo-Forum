9a7c9e89de97d413bd4b6136a9c4ab4284e93759
Henry <henry81325@gmail.com>
2018-10-13 14:24:09 +0800

    adjust reply function

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,33 +1,38 @@
 class RepliesController < ApplicationController
-  before_action :set_post
+  before_action :authenticate_user!
   before_action :set_reply, only: [:destroy, :update, :edit]
+  before_action :set_post
+  # before_action :authenticate_permit_user
 
   def create
-    @reply = @post.replies.create(reply_params)
+    @reply = @post.replies.build(reply_params)
     @reply.user = current_user
     if @reply.save
-      flash[:notice] = "留言成功！"
+      flash[:notice] = "Reply success！"
       redirect_to post_path(@post)
     else
-      flash[:alert] = "留言不可以空白!!"
+      @post = Post.find(params[:post_id])     
+      flash[:alert] = "Reply can't be blank or too long!!"
       redirect_to post_path(@post)
     end
   end
 
   def update
     if @reply.update(reply_params)
-      flash[:notice] = '留言已更新'
+      flash[:notice] = 'Successfully updated'
     else
       flash[:alert] = @reply.errors.full_messages.to_sentence if @reply.errors.any?
     end
     redirect_to post_path(@post)
   end
 
+
   def destroy
     @reply.destroy
-    flash[:notice] = "Successfully deleted"
+    flash[:alert] = "reply was deleted"  
     redirect_back(fallback_location: root_path)
   end
+
 
   private
 
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,5 +1,6 @@
 class Reply < ApplicationRecord
   validates_presence_of :comment
+  validates_length_of :comment, maximum: 300
 
   belongs_to :user
   belongs_to :post, counter_cache: true
